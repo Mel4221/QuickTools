615d7637c2b99ac5ab0e38a41d92896b3458175d
Mel4221 <melqui4251@gmail.com>
2022-10-06 00:30:23 -0400

    Update

--- binary.cs
+++ binary.cs
@@ -1,62 +1,151 @@
-/*
-
-      this is only an example this and should be commented entiredly 
-
-
-
-
+//
+// ${Melquiceded Balbi Villanueva}
+//
+// Author:
+//       ${Melquiceded} <${melquiceded.balbi@gmail.com}>
+//
+// Copyright (c) ${2089} MIT
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 using System;
 using System.IO;
-using System.Text;
+namespace QuickTools
 
-namespace Program
 {
-class ConsoleApplication
-{
-    const string fileName = "AppSettings.dat";
 
-  
-    public static void WriteDefaultValues()
-    {
-        using (var stream = File.Open(fileName, FileMode.Create))
-        {
-            using (var writer = new BinaryWriter(stream, Encoding.UTF8, false))
+      class Binary
+
+      {
+
+            public static bool CopyBinaryFile(string srcfilename, string destfilename)
+
             {
-                writer.Write(1.250F);
-                writer.Write(@"This data");
-                writer.Write(10);
-                writer.Write(true);
+
+
+
+                  if (File.Exists(srcfilename) == false)
+
+                  {
+
+                        Console.WriteLine("Could not find the Source file");
+
+                        return false;
+
+                  }
+
+
+
+                  Stream s1 = File.Open(srcfilename, FileMode.Open);
+
+                  Stream s2 = File.Open(destfilename, FileMode.Create);
+
+
+
+                  BinaryReader f1 = new BinaryReader(s1);
+
+                  BinaryWriter f2 = new BinaryWriter(s2);
+
+
+
+                  while (true)
+
+                  {
+
+                        byte[] buf = new byte[10240];
+
+                        int sz = f1.Read(buf, 0, 10240);
+
+                        if (sz <= 0)
+
+                              break;
+
+                        f2.Write(buf, 0, sz);
+
+                        if (sz < 10240)
+
+                              break; // eof reached
+
+                  }
+
+                  f1.Close();
+
+                  f2.Close();
+
+                  return true;
+
             }
-        }
-    }
 
-    public static void DisplayValues()
-    {
-        float aspectRatio;
-        string tempDirectory;
-        int autoSaveTime;
-        bool showStatusBar;
 
-        if (File.Exists(fileName))
-        {
-            using (var stream = File.Open(fileName, FileMode.Open))
+
+            public static bool CopyTextFile(string srcfilename, string destfilename)
+
             {
-                using (var reader = new BinaryReader(stream, Encoding.UTF8, false))
-                {
-                    aspectRatio = reader.ReadSingle();
-                    tempDirectory = reader.ReadString();
-                    autoSaveTime = reader.ReadInt32();
-                    showStatusBar = reader.ReadBoolean();
-                }
+
+
+
+                  if (File.Exists(srcfilename) == false)
+
+                  {
+
+                        Console.WriteLine("Could not find the Source file");
+
+                        return false;
+
+                  }
+
+
+
+                  StreamReader f1 = new StreamReader(srcfilename);
+
+                  StreamWriter f2 = new StreamWriter(destfilename);
+
+
+
+                  while (true)
+
+                  {
+
+                        char[] buf = new char[1024];
+
+                        int sz = f1.Read(buf, 0, 1024);
+
+                        if (sz <= 0)
+
+                              break;
+
+                        f2.Write(buf, 0, sz);
+
+                        if (sz < 1024)
+
+                              break; // eof reached
+
+                  }
+
+                  f1.Close();
+
+                  f2.Close();
+
+                  return true;
+
             }
 
-            Console.WriteLine("Aspect ratio set to: " + aspectRatio);
-            Console.WriteLine("Temp directory is: " + tempDirectory);
-            Console.WriteLine("Auto save time set to: " + autoSaveTime);
-            Console.WriteLine("Show status bar: " + showStatusBar);
-        }
-    }
-}
-}
 
-*/
+
+      }
+}
