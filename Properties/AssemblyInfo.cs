0ad4c77df06c03465093c5a3af538b366f5a36fb
Melquiceded <melqui4251@gmail.com>
2023-11-20 20:55:59 -0400

    Solution Fixed

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("QuickTools")]
+[assembly: AssemblyDescription("This is a C# Library Design to contain an array of tools for console applications and any other project that wish to just to get their projects running")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MBV")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("MIT")]
+[assembly: AssemblyTrademark("MIT")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("4000")]
+
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
