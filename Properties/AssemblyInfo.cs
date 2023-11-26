14ff9e0016dff6395ea6e173ab69d6a66c927a13
Melquiceded <melqui4251@gmail.com>
2023-11-26 19:03:26 -0400

    Update

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("QuickTools Assembly")]
-[assembly: AssemblyDescription("QuickTools Lybrary")]
+[assembly: AssemblyTitle("QuickTools")]
+[assembly: AssemblyDescription("This is a C# Library Design to contain an array of tools for console applications and any other project that wish to just to get their projects running")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MBV")]
-[assembly: AssemblyProduct("QuickTools Lybrary Assembly")]
+[assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("MIT")]
 [assembly: AssemblyTrademark("MIT")]
 [assembly: AssemblyCulture("")]
@@ -17,11 +17,10 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("3000.0.0")]
+[assembly: AssemblyVersion("4000")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
 
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
-//
